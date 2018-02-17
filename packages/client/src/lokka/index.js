29ee9beaf3fc89d9b611062cd0082e038d494d35
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-02-17 09:37:52 -0800

    feat(post): add the musings to the repo

--- packages/client/src/lokka/index.js
+++ packages/client/src/lokka/index.js
@@ -12,16 +12,45 @@ export default class LokkaComponent extends React.Component {
     this.client = new Lokka({
       transport: new Transport("/graphql")
     });
+
+    this.setPayload = this.setPayload.bind(this);
   }
 
   componentDidMount() {
     this.client.query(helloWorldQuery).then(data => {
       this.setState(data);
     });
+
+    // watch the query
+    const watchHandler = (err, payload) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+
+      this.setState(payload);
+    };
+
+    this.client.watchQuery(helloWorldQuery, {}, watchHandler);
+  }
+
+  setPayload() {
+    return this.client.cache.setItemPayload(
+      helloWorldQuery,
+      {},
+      {
+        helloWorld: "Hello World set via Cache"
+      }
+    );
   }
 
   render() {
     const { helloWorld } = this.state;
-    return <p>{helloWorld} from Lokka</p>;
+    return (
+      <section>
+        <p>{helloWorld} from Lokka</p>
+        <button onClick={this.setPayload}> Set item in Cache </button>
+      </section>
+    );
   }
 }
